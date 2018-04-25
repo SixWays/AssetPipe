5d0cd7aea202cc09a63fe805e0878aa89470a68d
Luke Thompson <>
2018-04-25 19:49:44 +0100

    Fixed helpers and examples for new struct design
    Merge branch 'temp'

--- Editor/AssetPipeHelpers.cs
+++ Editor/AssetPipeHelpers.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
+using Sigtrap.AssetPipe.Data;
 
 namespace Sigtrap.AssetPipe.Helpers {
 	#region Progress Bars
